485d56b178534a7a08c238fe0176f67fc2a70a44
Simon Bächler <simon@stellanera.com>
2016-03-26 11:17:11 +0100

    v0.1.0

--- setup.py
+++ setup.py
@@ -1,14 +1,31 @@
 # coding: utf-8
-from distutils.core import setup
+from __future__ import unicode_literals
+from setuptools import setup, find_packages
 
 setup(
-        name='universalimages',
-        version='0.1',
-        packages=['universalimages', 'universalimages.filters', 'universalimages.filters.xmp'],
-        url='https://github.com/sbaechler/thumbor-universalimages',
-        license='MIT',
-        keywords='rmd responsive',
-        author='Simon Bächler',
-        author_email='b@chler.com',
-        description='A Thumbor Filter that interprets the Universal Images Metadata'
+    name='universalimages',
+    version='0.1.0',
+    packages=find_packages(exclude=['docs', 'tests']),
+    url='https://github.com/sbaechler/thumbor-universalimages',
+    license='MIT',
+    keywords='rmd responsive',
+    author='Simon Bächler',
+    author_email='b@chler.com',
+    install_requires=['thumbor'],
+    description='A Thumbor Filter that interprets the Universal Images Metadata',
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2.7',
+    ],
 )
